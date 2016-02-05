8b7020d1e57a11b2ae5bc4f7580e67f412b11217
Your Name <youremail@example.com>
2016-02-05 17:03:51 +0600

    AddedProject 2

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); final serial connection pulse
+        setContentView(R.layout.activity_main); final week
     }
 
     @Override
