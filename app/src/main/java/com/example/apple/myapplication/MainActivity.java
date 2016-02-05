89fc66d5201809bc39bb870b9c294d1b5cc7ce72
Your Name <youremail@example.com>
2016-02-05 16:39:07 +0600

    AddedProject 2

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); final change
+        setContentView(R.layout.activity_main); final meet
     }
 
     @Override
